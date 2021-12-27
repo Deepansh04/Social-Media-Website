0c8cf86982b9624444982e61f68008a9bf81a97a
Deepansh04 <deepansh9499409309@gmail.com>
2021-12-27 17:55:31 +0530

    My setup for project

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app=express();
 // port 80 run by default or when we deployed
 const port = 8000;
 
+// use express router (middleware)
+// '/' for any url
+app.use('/',require('./routes/index.js'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in runnin server : ${err}`);
