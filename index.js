cdfd414cdf5225b871db284b244abed117b2d966
Deepansh04 <deepansh9499409309@gmail.com>
2022-01-06 18:33:11 +0530

    added sign up

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express= require('express');
+const cookieParser=require('cookie-parser');
 const app=express();
 // port 80 run by default or when we deployed
 const port = 8000;
-
+const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+const { urlencoded } = require('express');
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(express.static('./assets'))
+app.use(expressLayouts); // by default layout.ejs is naming used for layout
 // use express router (middleware)
 // '/' for any url
 app.use('/',require('./routes/index.js'));
+// extract styles and script from subpages into layoutc
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 app.set('view engine','ejs');
 app.set('views','./views');
