ff196c6dd1068da2c8cacc7f1f0fe903b45b1a35
Deepansh04 <deepansh9499409309@gmail.com>
2022-01-21 23:35:35 +0530

    added css

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,6 @@ console.log('users');
 const usersController=require('../controllers/users_controller');
 router.get('/profile/:id',passport.checkAuthentication,usersController.profile);
 router.post('/update/:id',passport.checkAuthentication,usersController.update);
-
-
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
 
