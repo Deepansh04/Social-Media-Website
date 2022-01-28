37c5653a80628a97b3486ae2a3ad707e693ce993
Deepansh04 <deepansh9499409309@gmail.com>
2022-01-28 18:52:35 +0530

    added delete post

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -14,6 +14,8 @@
                     console.log(data);
                     let newPost= newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
+                    
                 },
                 error: function(error){
                     console.log(error.responseText);
@@ -29,7 +31,7 @@
              <p>
 
                      <small>
-                         <a class="delete-post-button" href="/posts/destroy/${post.id}">X</a>
+                         <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
                      </small>
                    
                 ${post.content }
@@ -61,6 +63,23 @@
          
      </li>`)
      }
+    // method to delete a post from DOM
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                },error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+
+        });
+    }
 
 createPost();
 
