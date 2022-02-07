bf09f387a34962ec0512ec471885d6513bba714e
Deepansh04 <deepansh9499409309@gmail.com>
2022-02-07 19:27:20 +0530

    added mail  feature

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,5 +1,6 @@
-const Comment = require("../models/comment");
+const Comment = require('../models/comment');
 const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailer');
 
 module.exports.create = async function(req, res){
 
@@ -15,6 +16,22 @@ module.exports.create = async function(req, res){
 
             post.comments.push(comment);
             post.save();
+            
+            comment = await comment.populate('user', 'name email');
+       
+            commentsMailer.newComment(comment);
+            if (req.xhr){
+                
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+
             req.flash('success', 'Comment published!');
 
             res.redirect('/');
@@ -39,19 +56,85 @@ module.exports.destroy = async function(req, res){
             comment.remove();
 
             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
             req.flash('success', 'Comment deleted!');
+
             return res.redirect('back');
         }else{
             req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
     }catch(err){
-        console.log('Error', err);
         req.flash('error', err);
         return;
     }
     
 }
+// const Comment = require("../models/comment");
+// const Post = require('../models/post');
+
+// module.exports.create = async function(req, res){
+
+//     try{
+//         let post = await Post.findById(req.body.post);
+
+//         if (post){
+//             let comment = await Comment.create({
+//                 content: req.body.content,
+//                 post: req.body.post,
+//                 user: req.user._id
+//             });
+
+//             post.comments.push(comment);
+//             post.save();
+//             req.flash('success', 'Comment published!');
+
+//             res.redirect('/');
+//         }
+//     }catch(err){
+//         req.flash('error', err);
+//         return;
+//     }
+    
+// }
+
+
+// module.exports.destroy = async function(req, res){
+
+//     try{
+//         let comment = await Comment.findById(req.params.id);
+
+//         if (comment.user == req.user.id){
+
+//             let postId = comment.post;
+
+//             comment.remove();
+
+//             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+//             req.flash('success', 'Comment deleted!');
+//             return res.redirect('back');
+//         }else{
+//             req.flash('error', 'Unauthorized');
+//             return res.redirect('back');
+//         }
+//     }catch(err){
+//         console.log('Error', err);
+//         req.flash('error', err);
+//         return;
+//     }
+    
+// }
 // module.exports.create = function (req,res){
 //     Post.findById(req.body.post,function(err,post){
        
