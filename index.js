a77ce2e1ef397ab6692b6fc0c6abe26bebb171f7
Deepansh04 <deepansh9499409309@gmail.com>
2022-02-15 21:47:43 +0530

    added chat box

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ const MongoStore = require('connect-mongo');
 const { urlencoded } = require('express');
 const flash= require('connect-flash');
 const customMware = require('./config/middleware');
+
+// setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is listening on port 5000');
+
 // const sassMiddleware=require('node-sass-middleware');
 // app.use(sassMiddleware({
 //     src:'./assets/scss', // from this folder
