119e14b4442bc6595c96e44c55a92889f9e76bd3
Deepansh04 <deepansh9499409309@gmail.com>
2022-04-25 19:10:32 +0530

    m

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
 const env=require('./environment');
-mongoose.connect(`mongodb://localhost/${env.db}`);
-
+//mongoose.connect(`mongodb://localhost/${env.db}`);
+mongoose.connect(process.env.MONGODB_URL);
 const db=mongoose.connection;
 
 db.on('error',console.error.bind(console,"error connecting to mongoDB"));
