fc82d777600baf8df5b6f78818e26e44037559f9
Deepansh04 <deepansh9499409309@gmail.com>
2022-04-26 20:08:43 +0530

    msg

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,9 +2,9 @@ const Comment = require('../models/comment');
 const Post = require('../models/post');
 const Like = require("../models/like");
 
-const commentsMailer = require('../mailers/comments_mailer');
-const queue = require('../config/kue');
-const commentEmailWorker = require('../workers/comment_email_worker');
+//const commentsMailer = require('../mailers/comments_mailer');
+//const queue = require('../config/kue');
+//const commentEmailWorker = require('../workers/comment_email_worker');
 
 module.exports.create = async function(req, res){
 
@@ -26,14 +26,14 @@ module.exports.create = async function(req, res){
             comment = await comment.populate('user', 'name email');
             // console.log(comment,"555555555555555");
             // commentsMailer.newComment(comment);
-            let job = queue.create('emails', comment).save(function(err){
-                if (err){
-                    console.log('Error in sending to the queue', err);
-                    return;
-                }
-                console.log('job enqueued', job.id);
+            // let job = queue.create('emails', comment).save(function(err){
+            //     if (err){
+            //         console.log('Error in sending to the queue', err);
+            //         return;
+            //     }
+            //     console.log('job enqueued', job.id);
 
-            });
+            // });
 
             if (req.xhr){
                 
